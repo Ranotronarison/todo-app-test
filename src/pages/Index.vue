df0b3372b458b71c73414a890124f83c0c1580db
Ranotronarison Nomena <arivonyran@gmail.com>
2019-12-28 15:55:11 +0300

    fetch todos from db

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,13 +20,17 @@
 
 <script>
 import todo from '../components/Todo'
-/* import exios from ''
- */export default {
+const DB = {
+  url: 'https://fc49457c-280f-4807-bf98-346c2a60d022-bluemix.cloudantnosqldb.appdomain.cloud/',
+  database: 'tododb'
+}
+
+export default {
   components: {
     todo
   },
-  beforeCreate: function () {
-
+  created: function () {
+    this.fetchTodos()
   },
   data: function () {
     return {
@@ -39,6 +43,33 @@ import todo from '../components/Todo'
     }
   },
   methods: {
+    fetchTodos () {
+      this.$axios(
+        {
+          method: 'post',
+          url: '/tododb/_find',
+          baseURL: DB.url,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          data: {
+            selector: {
+              user: 'Nomena'
+            },
+            fields: ['_id', '_rev', 'name']
+          },
+          auth: {
+            'password': '6243a2d127a35b31a780eb39aaceed40fa629108cd8c52bd31c76d20fdb9ec6d',
+            'username': 'fc49457c-280f-4807-bf98-346c2a60d022-bluemix'
+          }
+        }
+      ).then((response) => {
+        this.todos = response.data.docs
+      }).catch(function (error) {
+        console.log(error.message)
+      })
+    },
     onSubmit: function () {
       if (this.todoName.trim() && this.todoName.length > 0) {
         this.todos.push({ id: this.key, name: this.todoName, finished: false })
